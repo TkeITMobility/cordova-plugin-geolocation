37e460ecfb184c1d2baccaf8c4f8f768915a661f
Bryan Antigua <antigua.b@gmail.com>
2015-12-18 10:10:38 -0800

    moved when locations start

--- src/ios/CDVLocation.m
+++ src/ios/CDVLocation.m
@@ -132,12 +132,6 @@
         return;
     }
 #endif
-
-    // Tell the location manager to start notifying us of location updates. We
-    // first stop, and then start the updating to ensure we get at least one
-    // update, even if our location did not change.
-    [self.locationManager stopUpdatingLocation];
-    [self.locationManager startUpdatingLocation];
     __locationStarted = YES;
 
     if (enableHighAccuracy) {
